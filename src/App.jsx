48ad7e416a68695696017f7ddef514eaefa0ac72
Qasim <mianqasim065@gmail.com>
2025-08-23 01:30:24 +0500

    Fix: Memoized the Main componenet so it don't re-render on it's parent component re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostContext";
 
@@ -77,14 +77,14 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+const Main=memo(function Main() {
   return (
     <main>
       <FormAddPost />
       <Posts />
     </main>
   );
-}
+}) 
 
 function Posts() {
   return (
